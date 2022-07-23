c7204c502b1a05e35dc649c4f8d515a0e5fc10db
Justin LeFebvre <jstnlefebvre@gmail.com>
2022-07-23 16:01:18 -0400

    initial commit

---
+++ BodyWeightTracker/src/Shared/Shared.fs
@@ -0,0 +1,21 @@
+namespace Shared
+
+open System
+
+type Todo = { Id: Guid; Description: string }
+
+module Todo =
+    let isValid (description: string) =
+        String.IsNullOrWhiteSpace description |> not
+
+    let create (description: string) =
+        { Id = Guid.NewGuid()
+          Description = description }
+
+module Route =
+    let builder typeName methodName =
+        $"/api/%s{typeName}/%s{methodName}"
+
+type ITodosApi =
+    { getTodos: unit -> Async<Todo list>
+      addTodo: Todo -> Async<Todo> }
