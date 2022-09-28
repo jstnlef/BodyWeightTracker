120ae4bf634ebae8a75c09192a48ce4cb6cb8ab9
Justin LeFebvre <jstnlefebvre@gmail.com>
2022-09-28 15:24:59 -0400

    Adding ideal body weight estimate calculation
    # Explain why this change is being made
    
    # Provide links to any relevant tickets, articles or other resources

--- BodyWeightTracker/src/Client/Index.fs
+++ BodyWeightTracker/src/Client/Index.fs
@@ -23,33 +23,36 @@ let todosApi =
 let init () : Model * Cmd<Msg> =
     let weights =
         [| { date = DateTime.Now
+             weight = 214.2<lbs>
+             bodyFatPercent = Some 28.7 }
+           { date = DateTime.Now - TimeSpan.FromDays(1)
              weight = 222.4<lbs>
              bodyFatPercent = Some 30.1 }
-           { date = DateTime.Now
+           { date = DateTime.Now - TimeSpan.FromDays(2)
              weight = 224.4<lbs>
              bodyFatPercent = Some 30.1 }
-           { date = DateTime.Now - TimeSpan.FromDays(1)
+           { date = DateTime.Now - TimeSpan.FromDays(3)
              weight = 225.6<lbs>
              bodyFatPercent = Some 30.1 }
-           { date = DateTime.Now - TimeSpan.FromDays(2)
+           { date = DateTime.Now - TimeSpan.FromDays(4)
              weight = 222.2<lbs>
              bodyFatPercent = Some 30.1 }
-           { date = DateTime.Now - TimeSpan.FromDays(3)
+           { date = DateTime.Now - TimeSpan.FromDays(5)
              weight = 222.3<lbs>
              bodyFatPercent = Some 30.1 }
-           { date = DateTime.Now - TimeSpan.FromDays(4)
+           { date = DateTime.Now - TimeSpan.FromDays(6)
              weight = 224.6<lbs>
              bodyFatPercent = Some 30.1 }
-           { date = DateTime.Now - TimeSpan.FromDays(5)
+           { date = DateTime.Now - TimeSpan.FromDays(7)
              weight = 223.5<lbs>
              bodyFatPercent = Some 30.1 }
-           { date = DateTime.Now - TimeSpan.FromDays(6)
+           { date = DateTime.Now - TimeSpan.FromDays(8)
              weight = 224.4<lbs>
              bodyFatPercent = Some 30.1 } |]
 
     let model =
         { weights = weights
-          user = { height = 69.0<inch>; age = 34<year> } }
+          user = { height = 69.0<inch>; age = 35<year> } }
 
     //    let cmd = Cmd.OfAsync.perform todosApi.getTodos () GotTodos
 
@@ -94,6 +97,14 @@ let update (msg: Msg) (model: Model) : Model * Cmd<Msg> =
 
 let calculateBMI (height: float<inch>) (weight: float<lbs>) = 703.0 * (weight / (height * height))
 
+let estimateIdealWeight (data: DataPoint) (idealBodyFatPercent: float) : float<lbs> option =
+    data.bodyFatPercent
+    |> Option.map (fun bodyFatPercent ->
+        let currentBodyFat = bodyFatPercent / 100.0
+        let baseWeight = data.weight * (1.0 - currentBodyFat)
+        let idealBodyFat = idealBodyFatPercent / 100.0
+        baseWeight / (1.0 - idealBodyFat))
+
 let view (model: Model) (dispatch: Msg -> unit) =
     Bulma.container [
         Navbar.navbar
@@ -124,6 +135,20 @@ let view (model: Model) (dispatch: Msg -> unit) =
                     StatusBox.statusBox
                         { text = $"{bodyFat}"
                           subtext = "Most recent Body Fat Percentage" }
+                ]
+
+                Bulma.column [
+                    let idealLower =
+                        estimateIdealWeight weight 10.0
+                        |> Option.defaultValue 0.0<lbs>
+
+                    let idealHigher =
+                        estimateIdealWeight weight 15.0
+                        |> Option.defaultValue 0.0<lbs>
+
+                    StatusBox.statusBox
+                        { text = $"%0.1f{idealLower}-%0.1f{idealHigher}"
+                          subtext = "Ideal weight range by estimated Body Fat" }
                 ]
             ]
         ]
