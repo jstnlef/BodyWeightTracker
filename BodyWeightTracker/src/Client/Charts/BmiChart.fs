f19f10fbf04103477e628dc2b9d2704ee166cbfb
Justin LeFebvre <jstnlefebvre@gmail.com>
2022-11-13 00:41:12 -0500

    Add the BMI chart
    # Explain why this change is being made
    
    # Provide links to any relevant tickets, articles or other resources

--- BodyWeightTracker/src/Client/Charts/BmiChart.fs
+++ BodyWeightTracker/src/Client/Charts/BmiChart.fs
@@ -9,16 +9,80 @@ type BmiDataPoint =
     bmi: float<lbs / inch^2> }
 
 let bmiChart (user: User) (data: DataPoint array) =
-  Plotly.plot [ plot.traces [ traces.scatter [ scatter.x [ 1; 2; 3; 4 ]
-                                               scatter.y [ 10; 15; 13; 17 ]
-                                               scatter.mode.markers ]
-                              traces.scatter [ scatter.x [ 2; 3; 4; 5 ]
-                                               scatter.y [ 16; 5; 11; 9 ]
-                                               scatter.mode.lines ]
-                              traces.scatter [ scatter.x [ 1; 2; 3; 4 ]
-                                               scatter.y [ 12; 9; 15; 12 ]
+  let earliest = DateTime.UtcNow.AddDays(-3)
+  let latest = DateTime.UtcNow.AddDays(1)
+
+  let dates =
+    [| DateTime.UtcNow.AddDays(-2)
+       DateTime.UtcNow.AddDays(-1)
+       DateTime.UtcNow |]
+
+  Plotly.plot [ plot.traces [ traces.scatter [ scatter.x [| earliest; latest |]
+                                               scatter.y [ 16; 16 ]
+                                               scatter.fillcolor "#ebaaa3"
+                                               scatter.fill.tozeroy
+                                               scatter.mode.none ]
+                              traces.scatter [ scatter.x [| earliest; latest |]
+                                               scatter.y [ 18.5; 18.5 ]
+                                               scatter.fillcolor "#f3eac2"
+                                               scatter.fill.tonexty
+                                               scatter.mode.none ]
+                              traces.scatter [ scatter.x [| earliest; latest |]
+                                               scatter.y [ 25; 25 ]
+                                               scatter.fillcolor "#c1dad4"
+                                               scatter.fill.tonexty
+                                               scatter.mode.none ]
+                              traces.scatter [ scatter.x [| earliest; latest |]
+                                               scatter.y [ 30; 30 ]
+                                               scatter.fillcolor "#f3eac2"
+                                               scatter.fill.tonexty
+                                               scatter.mode.none ]
+                              traces.scatter [ scatter.x [| earliest; latest |]
+                                               scatter.y [ 35; 35 ]
+                                               scatter.fillcolor "#f2c5a2"
+                                               scatter.fill.tonexty
+                                               scatter.mode.none ]
+                              traces.scatter [ scatter.x [| earliest; latest |]
+                                               scatter.y [ 40; 40 ]
+                                               scatter.fillcolor "#eeb09b"
+                                               scatter.fill.tonexty
+                                               scatter.mode.none ]
+                              traces.scatter [ scatter.x [| earliest; latest |]
+                                               scatter.y [ 60; 60 ]
+                                               scatter.fillcolor "#ebaaa3"
+                                               scatter.fill.tonexty
+                                               scatter.mode.none ]
+                              traces.scatter [ scatter.name "BMI"
+                                               scatter.x dates
+                                               scatter.y [ 31.0; 30.4; 30.2 ]
                                                scatter.mode [ scatter.mode.lines
-                                                              scatter.mode.markers ] ] ] ]
+                                                              scatter.mode.markers ]
+                                               scatter.line [ line.color "#ee00d6" ] ] ]
+                plot.layout [ layout.title [ title.text "BMI over time" ]
+                              layout.xaxis [ xaxis.range [ earliest; latest ]
+                                             xaxis.rangeselector [ rangeselector.buttons [ buttons.button [ button.count
+                                                                                                              1
+                                                                                                            button.label
+                                                                                                              "1m"
+                                                                                                            button.step.month
+                                                                                                            button.stepmode.backward ]
+                                                                                           buttons.button [ button.count
+                                                                                                              3
+                                                                                                            button.label
+                                                                                                              "3m"
+                                                                                                            button.step.month
+                                                                                                            button.stepmode.backward ]
+                                                                                           buttons.button [ button.count
+                                                                                                              6
+                                                                                                            button.label
+                                                                                                              "6m"
+                                                                                                            button.step.month
+                                                                                                            button.stepmode.backward ]
+                                                                                           buttons.button [ button.step.all ] ] ]
+                                             xaxis.rangeslider [ rangeslider.range [ earliest; latest ] ]
+                                             xaxis.type'.date ]
+                              layout.yaxis [ yaxis.autorange.false'
+                                             yaxis.range [ 10; 60 ] ] ] ]
 
 // let bmis =
 //   data
--- BodyWeightTracker/src/Client/Charts/WeightChart.fs
+++ BodyWeightTracker/src/Client/Charts/WeightChart.fs
@@ -5,37 +5,38 @@ open Feliz
 open Feliz.Plotly
 open Shared
 
-type DataPoints =
+type WeightData =
   { dates: DateTime array
     weights: float array
     bodyFatPercents: float array
     trendWeights: float array }
 
-let weightChart (dataPoints: DataPoint array) =
-  let data =
-    { dates =
-        [| DateTime(2022, 11, 7)
-           DateTime(2022, 11, 8)
-           DateTime(2022, 11, 9)
-           DateTime(2022, 11, 10)
-           DateTime(2022, 11, 11)
-           DateTime(2022, 11, 12) |]
-      weights =
-        [| 209.2
-           208.4
-           207.8
-           208.4
-           210.6
-           209.8 |]
-      bodyFatPercents = [||]
-      trendWeights =
-        [| 210.4
-           210.0
-           209.7
-           209.7
-           209.5
-           209.3 |] }
+let toChartWeightData (dataPoints: DataPoint array) =
+  { dates =
+      [| DateTime(2022, 11, 7)
+         DateTime(2022, 11, 8)
+         DateTime(2022, 11, 9)
+         DateTime(2022, 11, 10)
+         DateTime(2022, 11, 11)
+         DateTime(2022, 11, 12) |]
+    weights =
+      [| 209.2
+         208.4
+         207.8
+         208.4
+         210.6
+         209.8 |]
+    bodyFatPercents = [||]
+    trendWeights =
+      [| 210.4
+         210.0
+         209.7
+         209.7
+         209.5
+         209.3 |] }
 
+let weightChart (dataPoints: DataPoint array) =
+  let data = toChartWeightData dataPoints
   let earliest = data.dates[0]
   let latest = data.dates[-1]
 
@@ -45,18 +46,24 @@ let weightChart (dataPoints: DataPoint array) =
                                                scatter.y data.weights
                                                scatter.line [ line.color "#17BECF" ] ]
                               traces.scatter [ scatter.mode.lines
-                                               scatter.name "Trend"
+                                               scatter.name "7 Day Trend"
                                                scatter.x data.dates
                                                scatter.y data.trendWeights
                                                scatter.line [ line.color "#b21009"
                                                               line.shape.spline ] ] ]
-                plot.layout [ layout.title [ title.text "Your Weight" ]
+                plot.layout [ layout.title [ title.text "Your weight over time" ]
                               layout.xaxis [ xaxis.autorange.true'
                                              xaxis.range [ earliest; latest ]
                                              xaxis.rangeselector [ rangeselector.buttons [ buttons.button [ button.count
                                                                                                               1
                                                                                                             button.label
                                                                                                               "1m"
+                                                                                                            button.step.month
+                                                                                                            button.stepmode.backward ]
+                                                                                           buttons.button [ button.count
+                                                                                                              3
+                                                                                                            button.label
+                                                                                                              "3m"
                                                                                                             button.step.month
                                                                                                             button.stepmode.backward ]
                                                                                            buttons.button [ button.count
